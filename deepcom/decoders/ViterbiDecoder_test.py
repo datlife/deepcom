2ba944c7d1117fd418557d45c5e79b96a52d7857
Dat <tdat.nguyen93@gmail.com>
2018-06-19 11:43:20 -0700

    Updated test

--- deepcom/decoders/ViterbiDecoder_test.py
+++ deepcom/decoders/ViterbiDecoder_test.py
@@ -1,4 +1,5 @@
 import numpy as np
+import commpy as comm
 from .ViterbiDecoder import ViterbiDecoder
 from ..encoders import ConvolutionalCodeEncoder
 from ..utils import generate_random_binary_sequence, corrupt_signal
@@ -13,13 +14,13 @@ def test_viterbi_decoder_on_conv_codes():
     # For reproducability
     np.random.seed(2018)
 
-    # Create a random message bits
+    # Create not-random message bit sequences
     msg_bit_sequences = [None for _ in range(NUM_SEQS)]
     for i in range(NUM_SEQS):
         message_bits = generate_random_binary_sequence(BLOCK_LEN)
         msg_bit_sequences[i] = message_bits
     
-    # Define a Conv Code Encoding Scheme
+    # Define an Encoder using Conv Code Encoding Scheme
     encoder = ConvolutionalCodeEncoder(
         constraint_length=CONSTRAINT_LEN,
         data_rate=DATA_RATE)
@@ -30,23 +31,32 @@ def test_viterbi_decoder_on_conv_codes():
         coded_bits = encoder.encode(message_bits)
         encoded_sequences[idx] = coded_bits
 
-    # Simulate data corruption over AWGN Channel
-    corrupted_signals = corrupt_signal(
-        input_signal=np.asarray(encoded_sequences), 
-        noise_type='awgn')
+    # Simulates data corruption over AWGN Channel
+    # encoded_sequences = corrupt_signal(
+    #     input_signal=np.asarray(encoded_sequences), 
+    #     noise_type='awgn')
+    # noisy_signals = comm.channels.awgn(np.asarray(encoded_sequences), snr_dB)
 
-    # Decode signals using Viterbi 
+    # Decode signals using Viterbi Decoder
     decoded_signals = ViterbiDecoder(
-        corrupted_signals, 
+        encoded_sequences, 
         constrain_length=CONSTRAINT_LEN, 
         rate=DATA_RATE)
+    
+    # Extract only BLOCK_LEN size from Viterbi outputs
+    decoded_signals = np.array(decoded_signals)[:, :BLOCK_LEN]
 
-    exptected_encoded_bits =  np.array(
-    [[0, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1],
-    [0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0],
-    [1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0],
-    [1, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 1, 0, 0],
-    [0, 0, 1, 1, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
+    # print(np.shape(decoded_signals))
+    # print(decoded_signals)
 
+    expected_msg_bit_sequences = np.array(
+    [[0, 1, 0, 1, 1, 0, 0, 0, 0, 1], 
+    [0, 0, 0, 1, 1, 0, 0, 1, 0, 0], 
+    [1, 1, 0, 1, 0, 1, 1, 0, 0, 0], 
+    [1, 1, 1, 0, 0, 1, 0, 1, 1, 0], 
+    [0, 1, 1, 1, 0, 0, 0, 0, 0, 0]])
 
-    assert np.all(np.asarray(decoded_signals) == exptected_encoded_bits)
+    assert np.all(np.asarray(decoded_signals) == expected_msg_bit_sequences)
+
+    # Because there are noises, we accept True 
+    # if most decode bits are matched with original msgs bit sequences.
