3ea1730564f227a6294abe62392897d4486fa78e
Dat <tdat.nguyen93@gmail.com>
2018-06-21 10:54:53 -0700

    @TODO: overfit one sample

--- train_rnn.py
+++ train_rnn.py
@@ -1,44 +1,74 @@
 """Train a neural decoder"""
 import argparse
+import numpy as np
 import tensorflow as tf
+from commpy.channelcoding import Trellis
+from sklearn.model_selection import train_test_split
+
+from deepcom.metrics import ber
+from deepcom.dataset import create_dataset
 from deepcom.NeuralDecoder import NRSCDecoder
 
-# SEED here
+
+def main(args):
+
+
+  #  Generator Matrix (octal representation)
+  G = np.array([[0o7, 0o5]]) 
+  M = np.array([3 - 1])
+  trellis = Trellis(M, G, feedback=0o7, code_type='rsc')
+
+  # ####################################
+  # Generate Dataset for training/eval
+  # ####################################
+  X_train, Y_train = create_dataset(
+      num_sequences=20,
+      block_length=args.block_length,
+      trellis=trellis,
+      snr=0.01,
+      seed=2018)
+
+  X_test, Y_test = create_dataset(
+      num_sequences=40,
+      block_length=args.block_length,
+      trellis=trellis,
+      snr=0.01,
+      seed=1111)
+
+  print('Number of training sequences {}'.format(len(X_train)))
+  print('Number of testing sequences {}'.format(len(X_test)))
+  print(X_train.shape, Y_train.shape)
+
+  # ####################################
+  # Define Neural Decoder Model
+  # ####################################
+  inputs = tf.keras.Input(shape=(None, 2))
+  outputs = NRSCDecoder(inputs, is_training=True)
+  model = tf.keras.Model(inputs, outputs)
+
+  model.summary()
+  model.compile(
+      optimizer=tf.keras.optimizers.Adam(args.learning_rate), 
+      loss='binary_crossentropy',
+      metrics=['acc'])
+
+  model.fit(
+      X_train, 
+      Y_train,
+      batch_size=args.batch_size)
+
+
 
 def parse_args():
   """Parse Arguments for training Neural-RSC.
     NOTE: Default hyper-parameters are from suggested the original paper.
   """
   args = argparse.ArgumentParser(description='Train a Neural Decoder')
-  args.add_argument('--batch_size', type=int, default=200)
+  args.add_argument('--batch_size', type=int, default=4)
   args.add_argument('--block_length', type=int, default=100)
   args.add_argument('--num_examples', type=int, default=12000)
   args.add_argument('--learning_rate', type=float, default=1e-3)
   return args.parse_args()
-
-
-def main(args):
-  print("Nahh")
-
-
-def input_fn(inputs, labels, batch_size):
-  """Data generator for neural decoder"""
-  dataset = tf.data.Dataset.from_tensor_slices((inputs, labels))
-  dataset = dataset.shuffle(1000).repeat().batch(batch_size)
-  return dataset
-
-
-def model_fn(inputs, labels, mode, params):
-  """Define Training Pipeline for Neural Decoder"""
-  is_training = (mode == tf.estimator.ModeKeys.TRAIN)
-  decoded_signals = NRSCDecoder(inputs, is_training)
-
-  # Compute Loss
-  loss = (decoded_signals - inputs)**2
-
-  # TODO
-  raise NotImplementedError
-
 
 
 if __name__ == '__main__':
