d174b1eebd59a8672ec7ec64049e0fdedd0c741f
Dat <tdat.nguyen93@gmail.com>
2018-06-23 18:44:05 -0700

    added documentation

--- generate_synthetic_dataset.py
+++ generate_synthetic_dataset.py
@@ -1,8 +1,8 @@
 """This script generates synthetic data for training/testing
-Neural Decoder. 
+Neural Decoder.
 
 Given a ground truth message bits Y, we simulates input signal
-passing through a Simple Modulation ( 0 --> 0, 1 --> 1 ) and 
+passing through a Simple Modulation ( 0 --> 0, 1 --> 1 ) and
 Additive White Gaussian Noise (AWGN) Channel to become X, input
 to the RNN (Neural) Decoder.
 
@@ -14,7 +14,7 @@ Example Usage:
 >>python generate_synthetic_dataset.py \
 --snr 0 \
 --block_length 1000 \
---num_training_sequences 0 \
+--num_training_sequences 12000\
 --num_testing_sequences  10000  \
 --num_cpu_cores 8 \
 --training_seed 2018 \
@@ -23,34 +23,34 @@ Example Usage:
 """
 import pickle
 import argparse
-import random
+import multiprocessing as mp
+
 import numpy as np
 import commpy as cp
-import multiprocessing as mp
 from commpy.channelcoding import Trellis
-
 from deepcom.utils import generate_message_bits, corrupt_signal
 
 def parse_args():
   """Parse Arguments for training Neural-RSC."""
-  args = argparse.ArgumentParser(description='Generate sythetic data for training neural decoder')
+  args = argparse.ArgumentParser(
+      description='Generate sythetic data for training neural decoder')
   args.add_argument('--block_length', type=int, default=100)
   args.add_argument('--num_cpu_cores', type=int, default=4)
   args.add_argument('--num_training_sequences', type=int, default=0)
   args.add_argument('--num_testing_sequences', type=int, default=0)
   args.add_argument('--training_seed', type=int, default=2018)
   args.add_argument('--testing_seed', type=int, default=1111)
-  args.add_argument('--snr', type=float,  nargs="*", default=[0.0, 1.0])
+  args.add_argument('--snr', type=float, nargs="*", default=[0.0, 1.0])
   return args.parse_args()
 
 
 def run(args):
   #  Generator Matrix (octal representation)
-  G = np.array([[0o7, 0o5]]) 
+  G = np.array([[0o7, 0o5]])
   M = np.array([3 - 1])
   trellis = Trellis(M, G, feedback=0o7, code_type='rsc')
 
-  X_train, Y_train, X_test, Y_test = [], [], [] ,[]
+  X_train, Y_train, X_test, Y_test = [], [], [], []
   # ####################################
   # Generate Dataset for training/eval
   # ####################################
@@ -58,8 +58,7 @@ def run(args):
     snr_train = min(min(args.snr), 1)
     print('Generating training data:')
     print('Numer of sequences: {} Block length={} SNR={}...\n'.format(
-        args.num_training_sequences, args.block_length, snr_train))  
-
+        args.num_training_sequences, args.block_length, snr_train))
     X_train, Y_train = create_dataset(
         num_sequences=args.num_training_sequences,
         block_length=args.block_length,
@@ -67,12 +66,10 @@ def run(args):
         snr=snr_train,
         seed=args.training_seed,
         num_cpus=args.num_cpu_cores)
-
   if args.num_testing_sequences > 0:
     print('Generating testing data:')
     print('Numer of sequences: {} Block length={} SNR={}...\n'.format(
-        args.num_testing_sequences, args.block_length, args.snr))  
-
+        args.num_testing_sequences, args.block_length, args.snr))
     X_test, Y_test = create_dataset(
         num_sequences=args.num_testing_sequences,
         block_length=args.block_length,
@@ -80,55 +77,52 @@ def run(args):
         snr=args.snr,
         seed=args.testing_seed,
         num_cpus=args.num_cpu_cores)
-
   print('Number of training sequences {}'.format(len(X_train)))
   print('Number of testing sequences {}\n'.format(len(X_test)))
-
   # ####################################
   # Save data into pickle object
-  # ####################################    
+  # ####################################
   filename = 'rnn_{}k_bl{}_snr0.dataset'.format(
       int(args.num_training_sequences  / 1000), args.block_length)
   with open(filename, 'wb') as f:
-      pickle.dump([X_train, Y_train, X_test, Y_test], f)
-    
+    pickle.dump([X_train, Y_train, X_test, Y_test], f)
   print('Dataset is saved to %s' % filename)
 
 
 def create_dataset(num_sequences, block_length, trellis, snr, seed, num_cpus=4):
-    # Init seed
-    np.random.seed(seed)
-    snr = snr + 10 * np.log10(1./2.)
-    sigma = np.sqrt(1. / (2. * 10 **(snr / 10.)))
-    with mp.Pool(processes=num_cpus) as pool:
-      X, Y = zip(*pool.starmap(func, 
-          [(block_length,trellis, sigma) for _ in range(num_sequences)]))
-
-    np.random.seed()
-    X = np.reshape(X, (-1, block_length, 2))
-    Y = np.reshape(Y, (-1, block_length, 1))
-    return X, Y
+  """Generate synthetic message bits for training RNN"""
+  # Init seed
+  np.random.seed(seed)
+  snr = snr + 10 * np.log10(1./2.)
+  sigma = np.sqrt(1. / (2. * 10 **(snr / 10.)))
+  with mp.Pool(processes=num_cpus) as pool:
+    result = pool.starmap(
+        func,
+        [(block_length, trellis, sigma) for _ in range(num_sequences)])
+    X, Y = zip(*result)
+  np.random.seed()
+  X = np.reshape(X, (-1, block_length, 2))
+  Y = np.reshape(Y, (-1, block_length, 1))
+  return X, Y
 
 
 def func(block_length, trellis, sigma):
-    """Helper function to generate a pair of (input, label)
-    for training Neural Decoder.
-    """
-    if type(sigma) is np.ndarray:
-        sigma = np.random.choice(sigma)
+  """Helper function to generate a pair of (input, label)
+  for training Neural Decoder.
+  """
+  if isinstance(sigma, np.ndarray):
+    sigma = np.random.choice(sigma)
+  ground_truth = generate_message_bits(block_length)
 
-    ground_truth = generate_message_bits(block_length)
+  # Simulates data sent over AWGN channel
+  coded_bits = cp.channelcoding.conv_encode(ground_truth, trellis)
+  noisy_bits = corrupt_signal(coded_bits, noise_type='awgn', sigma=sigma)
 
-    # Simulates data sent over AWGN channel
-    coded_bits = cp.channelcoding.conv_encode(ground_truth, trellis)
-    noisy_bits = corrupt_signal(coded_bits, noise_type='awgn', sigma=sigma)
-    
-    # Ignore the last 4 bits        
-    input_signal = noisy_bits[: 2*block_length]
-    return input_signal, ground_truth
-
+  # Ignore the last 4 bits
+  input_signal = noisy_bits[: 2*block_length]
+  return input_signal, ground_truth
 
 
 if __name__ == '__main__':
-    args = parse_args()
-    run(args)
+  arguments = parse_args()
+  run(arguments)
--- train_rnn.py
+++ train_rnn.py
@@ -4,8 +4,8 @@ import argparse
 import pickle
 import tensorflow as tf
 
+from deepcom.model import NRSCDecoder
 from deepcom.dataset import data_genenerator
-from deepcom.NeuralDecoder import NRSCDecoder
 from deepcom.metrics import BER, BLER, TrainValTensorBoard
 
 
@@ -23,8 +23,9 @@ def parse_args():
   args.add_argument('--num_hidden_units', type=int, default=400)
   return args.parse_args()
 
-def training_pipeline(args):
 
+def training_pipeline(args):
+  """Main Tranining Pipeline"""
   # Define path to save training log for visualization later
   experiment_log = './logs/BiGRU-{}-{}::dropout-{}::epochs-{}'.format(
       args.num_layers, args.num_hidden_units, args.dropout_rate, args.epochs)
@@ -40,40 +41,42 @@ def training_pipeline(args):
   # ####################################
   # Define Neural Decoder Model
   # ####################################
-  # Construct 
+  # Construct Model
   inputs = tf.keras.Input(shape=(None, 2))
-  outputs = NRSCDecoder(inputs, 
-      is_training=True, 
-      num_layers=args.num_layers, 
+  outputs = NRSCDecoder(
+      inputs,
+      is_training=True,
+      num_layers=args.num_layers,
       hidden_units=args.num_hidden_units)
   model = tf.keras.Model(inputs, outputs)
   model.compile(
-      optimizer=tf.keras.optimizers.Adam(args.learning_rate), 
+      optimizer=tf.keras.optimizers.Adam(args.learning_rate),
       loss='binary_crossentropy',
       metrics=[BER, BLER])
 
   # Attempt to load pretrained model if available.
   try:
     model_path = os.path.join(experiment_log, 'BiGRU.hdf5')
-    pretrained = tf.keras.models.load_model(model_path, custom_objects={'BER': BER, 'BLER': BLER})
+    pretrained = tf.keras.models.load_model(
+        model_path,
+        custom_objects={'BER': BER, 'BLER': BLER})
     model = pretrained
     print('Pre-trained weights are loaded.')
   except Exception as e:
     print(e)
-    pass
 
   # Setup some callbacks to help training better
   summary = TrainValTensorBoard(experiment_log, write_graph=False)
   backup = tf.keras.callbacks.ModelCheckpoint(
       filepath='%s/BiGRU.hdf5' % experiment_log,
-      monitor='val_BER', 
+      monitor='val_BER',
       save_best_only=True)
 
   # Stop training early if the model seems to overfit
   early_stoping = tf.keras.callbacks.EarlyStopping(
-      monitor='val_loss', 
-      min_delta=0.0, 
-      patience=3, 
+      monitor='val_loss',
+      min_delta=0.0,
+      patience=3,
       verbose=0, mode='auto')
 
   # ####################################
@@ -83,13 +86,14 @@ def training_pipeline(args):
   test_set = data_genenerator(X_test, Y_test, args.batch_size, shuffle=False)
 
   model.fit(
-      train_set.make_one_shot_iterator(), 
-      steps_per_epoch=len(Y_train) // args.batch_size, 
+      train_set.make_one_shot_iterator(),
+      steps_per_epoch=len(Y_train) // args.batch_size,
       validation_data=test_set.make_one_shot_iterator(),
       validation_steps=len(Y_test) // args.batch_size,
       callbacks=[summary, backup, early_stoping],
       epochs=args.epochs)
   print('Training is completed.')
+
 
 if __name__ == '__main__':
   arguments = parse_args()
